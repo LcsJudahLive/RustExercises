e45dab9bbb6fd6bb6c1904737638200bd989995a
Lucas Santos <lucassantosdosanjos@yahoo.com>
2021-07-06 18:59:40 -0300

    Using service_fn to handle results

--- hyper-microservice/src/main.rs
+++ hyper-microservice/src/main.rs
@@ -1,6 +1,35 @@
-use hyper::{Body, Response, Server};
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use futures::{future, Future};
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::service::service_fn;
+
+// Implementing Future trait result hyper::Response<Body> or hyper::Error
+fn microservice_handler(req: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error> {
+    // r# is a multiline string in Rust
+    const INDEX: &'static str = r#"
+        <!doctype html>
+        <html>
+            <head>
+                <title>Rust Microservice</title>
+            </head>
+            <body>
+                <h3>Rust Microservice</h3>
+            </body>
+        </html>
+    "#;
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            future::ok(Response::new(INDEX.into()))
+        },
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            future::ok(response)
+        },
+    }
+}
 
 fn main() {
     
@@ -8,12 +37,10 @@ fn main() {
     let addr = ([0, 0, 0, 0], 8080).into();
     // Server instance, it returns Builder struct
     let builder = Server::bind(&addr);
+
     // Default handler to accept requests
-    let server = builder.serve(|| {
-        service_fn_ok(|_| {
-            Response::new(Body::from("Rust microservice..."))
-        })
-    });
+    // Using service_fn as handlers for request results
+    let server = builder.serve(|| service_fn(microservice_handler));
 
     // Dropping server errors
     let server = server.map_err(drop);
